8becb368e7c5958e2ed3ad9c1739b18d79fdf77d
Flávio Micheletti <flaviomicheletti@hotmail.com>
2015-07-27 15:24:12 -0300

    step 000

---
+++ step-000/foo_test.py
@@ -0,0 +1,18 @@
+#
+# Settings
+#
+from django.conf import settings
+
+settings.configure(
+    DEBUG = True,
+    ALLOWED_HOSTS = ['localhost'],
+    ROOT_URLCONF = 'foo',
+    MIDDLEWARE_CLASSES = (
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+)
+
+import django
+django.setup()
