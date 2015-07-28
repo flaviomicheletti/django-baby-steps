f317935d452cb530304503309a23531e849baeef
Flávio Micheletti <flaviomicheletti@hotmail.com>
2015-07-28 16:19:01 -0300

    step-005

---
+++ step-005/foo.py
@@ -0,0 +1,11 @@
+import settings
+import urls
+
+
+#
+# Terminal
+#
+if __name__ == "__main__":
+    import sys
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
