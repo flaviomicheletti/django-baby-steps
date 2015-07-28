f80d49d0b82dcf9c9675e75ac8f5116bb0b2794c
Flávio Micheletti <flaviomicheletti@hotmail.com>
2015-07-28 16:18:26 -0300

    step-000

---
+++ step-000/foo.py
@@ -0,0 +1,26 @@
+#
+# Settings
+#
+# from django.conf import settings
+
+# settings.configure(
+#     DEBUG = True,
+#     ALLOWED_HOSTS = ['localhost'],
+#     ROOT_URLCONF = '__main__',
+#     MIDDLEWARE_CLASSES = (
+#         'django.middleware.common.CommonMiddleware',
+#         'django.middleware.csrf.CsrfViewMiddleware',
+#         'django.middleware.clickjacking.XFrameOptionsMiddleware',
+#     ),
+# )
+
+import django
+django.setup()
+print(django.get_version())
+
+#
+# Terminal
+#
+# import sys
+# from django.core.management import execute_from_command_line
+# execute_from_command_line(sys.argv)
