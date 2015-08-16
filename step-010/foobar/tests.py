6f76531d8f7526342ddeba7188d57b2f1a5f24be
Flávio Micheletti <flaviomicheletti@hotmail.com>
2015-08-16 08:09:14 -0300

    step-010: tests

--- step-010/foobar/tests.py
+++ step-010/foobar/tests.py
@@ -1,16 +1,18 @@
 import foo.settings
-import unittest
+# import unittest
+from django.test import TestCase
 from django.test import Client
 
 def foo():
     cli = Client()
     return cli.get('/')
 
-class MyTest(unittest.TestCase):
+# class MyTest(unittest.TestCase):
+class MyTest(TestCase):
     def test(self):
         response = foo()
         self.assertEqual(200, response.status_code)
         self.assertEqual(b'Hello World', response.content)
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+    # unittest.main()
